17f586f1a06591101e4868deb6fab7ac7d2a7d86
Oluwashina <shinzbaba@gmail.com>
2023-10-12 19:57:00 +0100

    chore: reactjs and typescript practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+import InputField from './components/InputField';
+import { Todo } from './model';
+import TodoList from './components/TodoList';
 
-function App() {
+
+// type Person ={
+//   name: string,
+//   age?: number
+// }
+
+// interface Person{
+//   name: string,
+//   age?: number
+// }
+
+// let person : Person = {
+//   name: "Pyiush"
+// }
+
+// function printName(name: string){
+//   console.log(name)
+// }
+
+
+const App: React.FC = ()=> {
+
+  const [todo, setTodo] = useState<string>("")
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e:React.FormEvent) => {
+    e.preventDefault()
+
+    if(todo){
+      setTodos(
+        [...todos,{id: Date.now(),todo,isDone: false}]
+        )
+      setTodo("")
+    }
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
